6e43347981e8534c1c6cb2e98d932cf26f361137
Simonas Karpavičius <karpavicius.simonas@gmail.com>
2023-04-12 17:21:01 +0300

    Coin Collection

--- Assets/Collector.cs
+++ Assets/Collector.cs
@@ -1,6 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
-    
+    public float coins = 0;
+    public TMP_Text coinText;
+
+    private void Start()
+    {
+        coinText.text = coins.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            coins++;
+            coinText.text = coins.ToString();
+            Destroy(collision.gameObject);
+        }
+    }
 }
